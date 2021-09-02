a43cb2870ee391602cf8e3b22ee9058c0d08acd0
Anonymous-Chris <sk.niyara@gmail.com>
2021-09-01 21:01:49 -0500

    update browser router to hash router for gh pages reload fix

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'react-mdl/extra/material.css';
-import 'react-mdl/extra/material.js';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "react-mdl/extra/material.css";
+import "react-mdl/extra/material.js";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </HashRouter>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
